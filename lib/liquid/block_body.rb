16a99e62882a78bc9ac20b08346ea641aa96495d
Ashwin Maroli <ashmaroli@gmail.com>
2021-09-06 11:46:28 +0530

    Refactor `Liquid::BlockBody`

--- lib/liquid/block_body.rb
+++ lib/liquid/block_body.rb
@@ -17,38 +17,35 @@ module Liquid
 
     def parse(tokenizer, parse_context)
       parse_context.line_number = tokenizer.line_number
-      while token = tokenizer.shift
+      while (token = tokenizer.shift)
         next if token.empty?
 
-        case
-        when token.start_with?(TAGSTART)
+        if token.start_with?(TAGSTART)
           whitespace_handler(token, parse_context)
-          unless token =~ FullToken
-            raise_missing_tag_terminator(token, parse_context)
-          end
-          tag_name = $1
-          markup = $2
+          raise_missing_tag_terminator(token, parse_context) unless token =~ FullToken
+
+          tag_name = Regexp.last_match(1)
+          markup   = Regexp.last_match(2)
+
           # fetch the tag from registered blocks
-          unless tag = registered_tags[tag_name]
-            # end parsing if we reach an unknown tag and let the caller decide
-            # determine how to proceed
-            return yield tag_name, markup
-          end
+          tag = registered_tags[tag_name]
+
+          # end parsing if we reach an unknown tag and let the caller decide how to proceed
+          return yield tag_name, markup unless tag
 
           new_tag = tag.parse(tag_name, markup, tokenizer, parse_context)
           @blank &&= new_tag.blank?
           @nodelist << new_tag
-        when token.start_with?(VARSTART)
+        elsif token.start_with?(VARSTART)
           whitespace_handler(token, parse_context)
           @nodelist << create_variable(token, parse_context)
           @blank = false
         else
-          if parse_context.trim_whitespace
-            token.lstrip!
-          end
+          token.lstrip! if parse_context.trim_whitespace
           parse_context.trim_whitespace = false
+
           @nodelist << token
-          @blank &&= !!(token =~ WhitespaceOrNothing)
+          @blank &&= WhitespaceOrNothing.match?(token)
         end
         parse_context.line_number = tokenizer.line_number
       end
@@ -59,9 +56,7 @@ module Liquid
     def whitespace_handler(token, parse_context)
       if token[2] == WhitespaceControl
         previous_token = @nodelist.last
-        if previous_token.is_a? String
-          previous_token.rstrip!
-        end
+        previous_token.rstrip! if previous_token.is_a?(String)
       end
       parse_context.trim_whitespace = (token[-3] == WhitespaceControl)
     end
@@ -71,11 +66,11 @@ module Liquid
     end
 
     def render(context)
-      output = []
+      output = +""
       context.resource_limits.render_score += @nodelist.length
 
       idx = 0
-      while node = @nodelist[idx]
+      while (node = @nodelist[idx])
         case node
         when String
           check_resources(context, node)
@@ -86,9 +81,8 @@ module Liquid
           render_node_to_output(node, output, context, node.blank?)
           break if context.interrupt? # might have happened in a for-block
         when Continue, Break
-          # If we get an Interrupt that means the block must stop processing. An
-          # Interrupt is any command that stops block execution such as {% break %}
-          # or {% continue %}
+          # If we get an Interrupt that means the block must stop processing.
+          # An Interrupt is any command that stops block execution such as {% break %} or {% continue %}
           context.push_interrupt(node.interrupt)
           break
         else # Other non-Block tags
@@ -98,7 +92,7 @@ module Liquid
         idx += 1
       end
 
-      output.join
+      output
     end
 
     private
@@ -112,17 +106,14 @@ module Liquid
       raise e
     rescue UndefinedVariable, UndefinedDropMethod, UndefinedFilter => e
       context.handle_error(e, node.line_number)
-      output << nil
     rescue ::StandardError => e
-      line_number = node.is_a?(String) ? nil : node.line_number
+      line_number = node.line_number unless node.is_a?(String)
       output << context.handle_error(e, line_number)
     end
 
     def check_resources(context, node_output)
       context.resource_limits.render_length += node_output.length
-      return unless context.resource_limits.reached?
-
-      raise MemoryError.new("Memory limits exceeded")
+      raise MemoryError, "Memory limits exceeded" if context.resource_limits.reached?
     end
 
     def create_variable(token, parse_context)
@@ -134,15 +125,13 @@ module Liquid
     end
 
     def raise_missing_tag_terminator(token, parse_context)
-      raise SyntaxError.new(
-        parse_context.locale.t("errors.syntax.tag_termination", token: token, tag_end: TagEnd.inspect)
-      )
+      raise SyntaxError,
+            parse_context.locale.t("errors.syntax.tag_termination", token: token, tag_end: TagEnd.inspect)
     end
 
     def raise_missing_variable_terminator(token, parse_context)
-      raise SyntaxError.new(
-        parse_context.locale.t("errors.syntax.variable_termination", token: token, tag_end: VariableEnd.inspect)
-      )
+      raise SyntaxError,
+            parse_context.locale.t("errors.syntax.variable_termination", token: token, tag_end: VariableEnd.inspect)
     end
 
     def registered_tags
